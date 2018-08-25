bfc7ce5570b036ff1460dc249252c83addb60e7a
Yuxiang Dai <yuxiang.dai43@gmail.com>
2018-08-25 12:32:08 -0400

    Firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+var firebase = require("firebase");
+var c = require("./constants");
+
+const config = {
+  apiKey: c.FIREBASE_API_KEY,
+  authDomain: c.FIREBASE_AUTH_DOMAIN,
+  databaseURL: c.FIREBASE_DATABASE_URL,
+  projectId: c.FIREBASE_PROJECT_ID,
+  storageBucket: c.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: c.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+export const database = firebase.database();
+export const storage = firebase.storage();
