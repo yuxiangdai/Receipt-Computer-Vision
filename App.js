8a56e5b30484d22b37a84a2c3b9930f30afc8ecb
Yuxiang Dai <yuxiang.dai43@gmail.com>
2018-08-25 15:16:34 -0400

    Server and RN

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up Bruce.js to start working on your app!</Text>
+        <Text>Open up Zheng.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
--- src/server.js
+++ src/server.js
@@ -13,6 +13,51 @@ const host = process.env.APP_HOST || '127.0.0.1';
 
 const router = require('./routes');
 app.use('/', router);
+
+app.get('/test', function(req, res){
+  const vision = require('@google-cloud/vision');
+
+  const client = new vision.ImageAnnotatorClient();
+  const fileName = process.env.PWD + '/longos1.jpg';
+  
+  client
+    .textDetection(fileName)
+    .then(results => {
+      
+      const detections = results[0].textAnnotations;
+      console.log('Text:', detections.length);
+      let i = 0;
+      var string = "";
+      for(i = 1; i < detections.length; i++){
+        const text = detections[i];
+        console.log("OUT:"+ text.description)
+        string += text.description
+        //console.log("OUTRESULTS:" + resultString);
+        // console.log(text);
+        console.log(text.boundingPoly);
+
+        const verticies = text.boundingPoly.vertices;
+        let Y_average = 0
+        for(let j = 0; j < verticies.length; j++) {
+          Y_average += verticies[j].y;
+        }
+        
+        console.log(Y_average/4);
+      }
+      res.send(string);
+      console.log(detections[0].boundingPoly);
+      // detections.forEach(text => {
+      //   console.log(text);
+      //   console.log(text.description);
+      //   console.log(text.boundingPoly);
+      // });
+    })
+    .catch(err => {
+      console.error('ERROR:', err);
+    });
+});
+
 app.listen(port, host);
+
 
 console.log(`Server listening at ${host}:${port}`);
