da37df3c7c3136d39a0d2ada993606c4e0b8c345
Yuxiang Dai <yuxiang.dai43@gmail.com>
2018-08-25 00:16:48 -0400

    Ejected

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const test = search => fetch(`${baseUrl.API_URL}/hotels/search/${search}`, {
+  method: 'GET',
+  headers: {
+    Accept: 'application/json',
+  },
+});
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Open up Bruce.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
