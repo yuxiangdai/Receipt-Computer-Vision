67219e5522107a4481c10388364531f460887c1b
Yuxiang Dai <yuxiang.dai43@gmail.com>
2018-08-25 20:13:27 -0400

    Routes

--- App.js
+++ App.js
@@ -1,8 +1,52 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import './stdlib/lib';
+import { Button, StatusBar, StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, SafeAreaView, createBottomTabNavigator} from 'react-navigation';
 
-export default class App extends React.Component {
+class ImagesScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Images',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="List"
+          onPress={() => this.props.navigation.navigate('List')}
+        />
+      </View>
+    );
+  }
+}
+
+class ListScreen extends React.Component {
+  static navigationOptions = {
+    title: 'List',
+  };
+
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>List Screen</Text>
+        <Button
+          title="Go to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+        <Button
+          title="Go to Images"
+          onPress={() => this.props.navigation.navigate('Images')}
+        />
+      </View>
+    );
+  }
+}
+
+class HomeScreen extends React.Component {
   constructor(props){
     super(props);
     this.state ={ res: "None"};
@@ -32,6 +76,15 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <Text>Home Screen</Text>
+        <Button
+          title="Go to List"
+          onPress={() => this.props.navigation.navigate('List')}
+        />
+        <Button
+          title="Go to Images"
+          onPress={() => this.props.navigation.navigate('Images')}
+        />
         <Text>{this.state.res}</Text>
       </View>
     );
@@ -46,3 +99,21 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+const RootStack = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    List: ListScreen,
+    Images: ImagesScreen,
+  },
+  {
+    initialRouteName: 'Home',
+  }
+);
+
+export default class App extends React.Component {
+  render() {
+    return <RootStack />;
+  }
+}
